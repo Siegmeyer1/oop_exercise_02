edffd7881810fe79d44f5cd56f97be234044e763
Siegmeyer1 <47855803+Siegmeyer1@users.noreply.github.com>
2019-09-30 17:03:56 +0300

    Add files via upload

--- FazzyNumber.cpp
+++ FazzyNumber.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstring>
-#include <math.h>
 #include <cassert>
 #include <sstream>
 #include "FazzyNumber.h"
@@ -26,34 +25,6 @@ Fazzy::Fazzy()
 
 Fazzy operator ""_seg(const char *str, size_t size) //[12.5:16.785]
 {
-    /*double left = 0;
-    double right = 0;
-    double *num;
-    num = &left;
-    char c;
-    int count = 0;
-    bool flag = false, sign_flag = false;
-    for (size_t i = 1; i < size-1; i++)
-    {
-        c = str[i];
-        if (c == ':') {
-            *num = *num / pow(10, count);
-            if (sign_flag) *num = -*num;
-            num = &right;
-            count = 0;
-            flag = false;
-        } else if (c == '-') {
-            sign_flag = true;
-        } else if (c == '.') {
-            flag = true;
-        } else {
-            if (flag) count++;
-            *num = *num * 10 + (c - '0');
-        }
-    }
-    *num = *num / pow(10, count);
-    if (sign_flag) *num = -*num;
-    return {left, right};*/
     std::istringstream is(str);
     char tmp;
     double l, r;
@@ -91,13 +62,7 @@ Fazzy operator*(const Fazzy& A, const Fazzy& B)
 
 Fazzy operator/(const Fazzy& A, const Fazzy& B)
 {
-    /*if ((B.l != 0) & (B.r != 0)) {
-        return {A.l / B.r, A.r / B.l};
-    } else {
-        std::cout << "Can`t divide by zero\n";
-        return {0, 0};
-    }*/
-    if (((B.l > 0) && (B.r > 0)) || ((B.l < 0) && (B.r < 0))) {
+    if (B.l * B.r > 0) {
         Fazzy res;
         double max, min, tmp[4];
         tmp[0] = A.l / B.l;
@@ -117,15 +82,7 @@ Fazzy operator/(const Fazzy& A, const Fazzy& B)
 
 Fazzy operator~(const Fazzy& A)
 {
-    /*if ((A.l != 0) & (A.r != 0))
-    {
-        return {1 / A.r, 1 / A.l};
-    } else if ((A.l == 0) & (A.r != 0)) {
-        return {A.r, 1 / A.l};
-    } else if ((A.l != 0) & (A.r == 0)) {
-        return {1 / A.r, A.l};
-    } else return {0, 0};*/
-    if ((A.l != 0) & (A.r != 0))
+    if (A.l * A.r > 0)
     {
         return {1 / A.r, 1 / A.l};
     } else assert(0);
