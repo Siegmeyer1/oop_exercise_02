aae592a01790d81a807ba9a36d0cfa51c260400e
Siegmeyer1 <47855803+Siegmeyer1@users.noreply.github.com>
2019-09-30 15:57:47 +0300

    Add files via upload

--- FazzyNumber.cpp
+++ FazzyNumber.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include <math.h>
+#include <cassert>
+#include <sstream>
 #include "FazzyNumber.h"
   
 
@@ -24,7 +26,7 @@ Fazzy::Fazzy()
 
 Fazzy operator ""_seg(const char *str, size_t size) //[12.5:16.785]
 {
-    double left = 0;
+    /*double left = 0;
     double right = 0;
     double *num;
     num = &left;
@@ -51,7 +53,12 @@ Fazzy operator ""_seg(const char *str, size_t size) //[12.5:16.785]
     }
     *num = *num / pow(10, count);
     if (sign_flag) *num = -*num;
-    return {left, right};
+    return {left, right};*/
+    std::istringstream is(str);
+    char tmp;
+    double l, r;
+    is >> tmp >> l >> tmp >> r;
+    return {l, r};
 }
 
 
@@ -67,29 +74,61 @@ Fazzy operator-(const Fazzy& A, const Fazzy& B)
 
 Fazzy operator*(const Fazzy& A, const Fazzy& B)
 {
-    return {A.l * B.l, A.r * B.r};
+    double max, min, tmp[4];
+    tmp[0] = B.l * A.l;
+    tmp[1] = B.l * A.r;
+    tmp[2] = B.r * A.l;
+    tmp[3] = B.r * A.r;
+    max = tmp[0];
+    min = tmp[3];
+    for (int i = 1; i < 4; i++)
+    {
+        if (tmp[i] > max) max = tmp[i];
+        if (tmp[3-i] < min) min = tmp[3-i];
+    }
+    return {min, max};
 }
 
 Fazzy operator/(const Fazzy& A, const Fazzy& B)
 {
-    if ((B.l != 0) & (B.r != 0)) {
+    /*if ((B.l != 0) & (B.r != 0)) {
         return {A.l / B.r, A.r / B.l};
     } else {
         std::cout << "Can`t divide by zero\n";
         return {0, 0};
-    }
+    }*/
+    if (((B.l > 0) && (B.r > 0)) || ((B.l < 0) && (B.r < 0))) {
+        Fazzy res;
+        double max, min, tmp[4];
+        tmp[0] = A.l / B.l;
+        tmp[1] = A.l / B.r;
+        tmp[2] = A.r / B.l;
+        tmp[3] = A.r / B.r;
+        max = tmp[0];
+        min = tmp[3];
+        for (int i = 1; i < 4; i++)
+        {
+            if (tmp[i] > max) max = tmp[i];
+            if (tmp[3-i] < min) min = tmp[3-i];
+        }
+        return {min, max};
+    } else assert(0);
 }
 
 Fazzy operator~(const Fazzy& A)
 {
-    if ((A.l != 0) & (A.r != 0))
+    /*if ((A.l != 0) & (A.r != 0))
     {
         return {1 / A.r, 1 / A.l};
     } else if ((A.l == 0) & (A.r != 0)) {
         return {A.r, 1 / A.l};
     } else if ((A.l != 0) & (A.r == 0)) {
         return {1 / A.r, A.l};
-    } else return {0, 0};
+    } else return {0, 0};*/
+    if ((A.l != 0) & (A.r != 0))
+    {
+        return {1 / A.r, 1 / A.l};
+    } else assert(0);
 }
 
 bool operator>(const Fazzy& A, const Fazzy& B)
